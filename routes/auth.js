8364e09586479383a85e7cdad35202c546b8a04f
Tomek Rozalski <alium@daucusetanser.pl>
2018-10-27 22:51:44 +0200

    Playing with authorization. It works

--- routes/auth.js
+++ routes/auth.js
@@ -7,32 +7,77 @@ const db = require('../db');
 const router = Router();
 
 const createToken = () => {
-	return jwt.sign({}, 'secret', { expiresIn: '1h' });
+	return jwt.sign({}, 'hophead', { expiresIn: '1h' });
 };
+
+const verifyToken = (req, res, next) => {
+	const bearerHeader = req.headers['authorization'];
+
+	if (typeof bearerHeader !== 'undefined') {
+		req.token = bearerHeader.split(' ')[1];
+		next();
+	} else {
+		res
+			.status(403)
+			.json({ message: 'Access forbidden' });
+	}
+};
+
+router.get('/test', verifyToken, (req, res) => {
+	jwt.verify(req.token, 'hophead', (err, authData) => {
+		if (err) {
+			res.sendStatus(403);
+		} else {
+			res
+				.status(200)
+				.json({
+					message: 'Test dziala! :D',
+					authData,
+				});
+		}
+	});
+});
 
 router.post('/login', (req, res, next) => {
 	const email = req.body.email;
 	const pw = req.body.password;
-	// Check if user login is valid
-	// If yes, create token and return it to client
-	const token = createToken();
-	// res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-	res
-		.status(401)
-		.json({ message: 'Authentication failed, invalid username or password.' });
+
+	db.getDb()
+		.db()
+		.collection('users')
+		.findOne({ email })
+		.then((userDoc) => {
+			console.log('userDoc', pw, userDoc.password);
+			return bcrypt.compare(pw, userDoc.password)
+		})
+		.then((result) => {
+			if (!result) {
+				throw Error();
+			}
+
+			const token = createToken();
+
+			res
+				.status(200)
+				.json({
+					message: 'Authentication succeeded.',
+					token,
+				});
+		})
+		.catch((err) => {
+			res
+				.status(401)
+				.json({ message: 'Authentication failed, invalid username or password.' });
+		});
 });
 
 router.post('/signup', (req, res, next) => {
-	console.log('--------------------------');
-	console.log('-->', req.body);
-	console.log('--------------------------');
 	const email = req.body.email;
 	const pw = req.body.password;
-	// Hash password before storing it in database => Encryption at Rest
+
 	bcrypt
 		.hash(pw, 12)
 		.then(hashedPW => {
-		// Store hashedPW in database
 			db.getDb()
 				.db()
 				.collection('users')
@@ -46,7 +91,7 @@ router.post('/signup', (req, res, next) => {
 					const token = createToken();
 					res
 						.status(201)
-						.json({ token: token, user: { email: email } });
+						.json({ token, user: { email } });
 				})
 				.catch((err) => {
 					console.log(err);
@@ -57,7 +102,6 @@ router.post('/signup', (req, res, next) => {
 			console.log(err);
 			res.status(500).json({ message: 'Creating the user failed.' });
 		});
-		// Add user to database
 });
 
 module.exports = router;
