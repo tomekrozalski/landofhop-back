f896b103badbe5058f33f6040bed9ab610875c48
Tomek Rozalski <alium@daucusetanser.pl>
2018-12-18 19:50:51 +0100

    Add new endpoint, POST /institutions

--- routes/institutions.js
+++ routes/institutions.js
@@ -1,9 +1,12 @@
 const Router = require('express').Router;
 const mongodb = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const ObjectId = mongodb.ObjectId;
 
 const db = require('../db');
+const verifyToken = require('../utils/verifyToken');
+
 const router = Router();
 
 router.get('/', (req, res, next) => {
@@ -76,6 +79,29 @@ router.get('/:id', (req, res, next) => {
 				.status(500)
 				.json({ message: 'An error occured' });
 		});
+});
+
+router.post('/', verifyToken, (req, res) => {
+	jwt.verify(req.token, process.env.JWT_SECRET, (err, authData) => {
+		if (err) {
+			res.sendStatus(403);
+		} else {
+			db.getDb()
+				.db()
+				.collection('institutions')
+				.insertOne(req.body)
+				.then((result) => {
+					res
+						.status(200)
+						.json(result);
+				})
+				.catch((err) => {
+					res
+						.status(500)
+						.json({ message: 'An error occured' });
+				});
+		}
+	});
 });
 
 module.exports = router;
