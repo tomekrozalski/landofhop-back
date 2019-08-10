fe0d81490cfe113fc7d73faba72aa67cf7747d67
Tomek Rozalski <alium@daucusetanser.pl>
2019-08-10 23:55:39 +0200

    [Mongoose refactor] Working on beverage gallery image upload. Introduce sharp. Copy image in Webp and smaller JPG works fine

--- routes/images.js
+++ routes/images.js
@@ -3,8 +3,8 @@ const { Router } = require('express');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const rimraf = require('rimraf');
+const sharp = require('sharp');
 
-const Country = require('../models/Country');
 const verifyToken = require('../utils/verifyToken');
 
 const router = Router();
@@ -20,14 +20,33 @@ function checkIfDirectoryExists(directory) {
 	}
 }
 
+function createForldersInsideContainer(containerDir) {
+	const originalImages = `${containerDir}/original`;
+	const largeImages = `${containerDir}/2x`;
+	const smallImages = `${containerDir}/1x`;
+	const largeJpg = `${containerDir}/2x/jpg`;
+	const largeWebp = `${containerDir}/2x/webp`;
+	const smallJpg = `${containerDir}/1x/jpg`;
+	const smallWebp = `${containerDir}/1x/webp`;
+
+	fs.mkdirSync(containerDir);
+	fs.mkdirSync(originalImages);
+	fs.mkdirSync(largeImages);
+	fs.mkdirSync(smallImages);
+	fs.mkdirSync(largeJpg);
+	fs.mkdirSync(largeWebp);
+	fs.mkdirSync(smallJpg);
+	fs.mkdirSync(smallWebp);
+
+	return true;
+}
+
 function checkIfGalleryExists({ badge, brand, shortId }) {
 	return new Promise((resolve) => {
 		const brandDir = `images/${brand}`;
 		const badgeDir = `images/${brand}/${badge}`;
 		const shortIdDir = `images/${brand}/${badge}/${shortId}`;
 		const containerDir = `images/${brand}/${badge}/${shortId}/container`;
-		const largeImages = `images/${brand}/${badge}/${shortId}/container/2x`;
-		const smallImages = `images/${brand}/${badge}/${shortId}/container/1x`;
 
 		checkIfDirectoryExists(brandDir);
 		checkIfDirectoryExists(badgeDir);
@@ -35,16 +54,12 @@ function checkIfGalleryExists({ badge, brand, shortId }) {
 
 		if (fs.existsSync(containerDir)) {
 			rimraf(containerDir, () => {
-				fs.mkdirSync(containerDir);
-				fs.mkdirSync(largeImages);
-				fs.mkdirSync(smallImages);
-				resolve();
+				createForldersInsideContainer(containerDir);
+				resolve(containerDir);
 			});
 		} else {
-			fs.mkdirSync(containerDir);
-			fs.mkdirSync(largeImages);
-			fs.mkdirSync(smallImages);
-			resolve();
+			createForldersInsideContainer(containerDir);
+			resolve(containerDir);
 		}
 	});
 }
@@ -52,9 +67,7 @@ function checkIfGalleryExists({ badge, brand, shortId }) {
 const fileStorage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		const { badge, brand, shortId } = req.params;
-		const largeImages = `images/${brand}/${badge}/${shortId}/container/2x`;
-
-		cb(null, largeImages);
+		cb(null, `images/${brand}/${badge}/${shortId}/container/original`);
 	},
 	filename: (req, file, cb) => {
 		cb(null, req.files.length < 10 ? `0${req.files.length}.jpg` : `${req.files.length}.jpg`);
@@ -71,13 +84,49 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ storage: fileStorage, fileFilter }).array('image');
 
-router.post('/beverage/gallery/:shortId/:brand/:badge', (req, res) => {
-	checkIfGalleryExists(req.params)
-		.then(() => {
-			upload(req, res, () => {
-				res.end('UPLOAD COMPLETED!');
-			});
-		});
+router.post('/beverage/gallery/:shortId/:brand/:badge', verifyToken, (req, res) => {
+	jwt.verify(req.token, process.env.JWT_SECRET, (authErr) => {
+		if (authErr) {
+			res.sendStatus(403);
+		} else {
+			checkIfGalleryExists(req.params)
+				.then((containerDir) => {
+					upload(req, res, () => {
+						const actualDirectory = `${containerDir}/original`;
+
+						fs.readdir(actualDirectory, (err, files) => {
+							if (err) {
+								return false;
+							}
+
+							files.forEach((file) => {
+								const [fileName] = file.split('.');
+
+								sharp(`${actualDirectory}/${file}`)
+									.resize(440)
+									.toFile(`${containerDir}/2x/jpg/${fileName}.jpg`);
+
+								sharp(`${actualDirectory}/${file}`)
+									.resize(440)
+									.toFile(`${containerDir}/2x/webp/${fileName}.webp`);
+
+								sharp(`${actualDirectory}/${file}`)
+									.resize(220)
+									.toFile(`${containerDir}/1x/jpg/${fileName}.jpg`);
+
+								sharp(`${actualDirectory}/${file}`)
+									.resize(220)
+									.toFile(`${containerDir}/1x/webp/${fileName}.webp`);
+							});
+
+							return true;
+						});
+
+						res.end('UPLOAD COMPLETED!');
+					});
+				});
+		}
+	});
 });
 
 module.exports = router;
